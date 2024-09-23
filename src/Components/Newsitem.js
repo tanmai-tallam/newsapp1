58e91b38abff265f5c74fb6f4d9a5e55309b83b0
Tanmai Tallam <tanmai.tallam@gmail.com>
2024-09-24 02:43:04 +0530

    new commit

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 
 export class Newsitems extends Component {
+
+
   render() {
+    // this is the syntax used to obtain the props(here its two props)
+    let {title, description, imageurl, newsurl }=this.props;
     return (
-      <div className="card" style={{width: "18rem;"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+      <div className="card" style={{width: "23rem"}}>
+        <img src={imageurl} style={{height:"13rem",width: "23rem"}} className="card-img-top" alt="nothing to display"/>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="/" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsurl} target="_blank" rel="noopener noreferrer" className="btn btn-md btn-primary">Read More</a>
+          {/* rel="noopener noreferrer": This attribute is important for security reasons when using target="_blank". It prevents the new page from having access to the window.opener object, which could potentially be exploited for security vulnerabilities (e.g., phishing attacks).
+          Without rel="noopener noreferrer", the site you open could manipulate the original page */}
         </div>
       </div>
     )
